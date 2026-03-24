05c7d7d2dacfea62af2c245bd6828faf0696c789
frcnko <fnarazaflores@danielcastelao.org>
2026-03-24 02:19:25 +0100

    hundir la flota

---
+++ juego.py
@@ -0,0 +1,30 @@
+from tablero import Tablero
+
+class Juego:
+    def __init__(self):
+        self.tablero = Tablero()
+        self.lanzar_ataque(1, 0)
+        self.lanzar_ataque(1, 0)
+        self.lanzar_ataque(1, 1)
+        self.lanzar_ataque(1, 2)
+        self.lanzar_ataque(5, 1)
+        self.lanzar_ataque(1, 4)
+
+    def mostrar_resultado(self, resultado):
+        if resultado == 0:
+            print("Agua")
+        elif resultado == 1:
+            print("Tocado")
+        elif resultado == 2:
+            print("Hundido")
+
+    def lanzar_ataque(self, x, y):
+        print(f"Ataque a {x},{y}")
+        casilla = self.tablero.casillero[x][y]
+        resultado = casilla.recibir_disparo()
+
+        if resultado is not None:
+            self.mostrar_resultado(resultado)
+
+
+juego = Juego()
---
+++ tablero.py
@@ -0,0 +1,31 @@
+from casilla import *
+
+class Tablero:
+    def __init__(self, tamanho=10):
+
+        self.AGUA = 0
+        self.TOCADO = 1
+        self.HUNDIDO = 2
+
+        por1 = Nave("Manuel", "portaaviones", 5)
+        fra1 = Nave("Damián", "fragata", 3)
+        fra2 = Nave("Elena", "fragata", 3)
+        fra3 = Nave("Noelia", "fragata", 3)
+
+        sub1 = Nave("U-47", "submarino", 1)
+        sub2 = Nave("U-96", "submarino", 1)
+        sub3 = Nave("U-505", "submarino", 1)
+        sub4 = Nave("U-534", "submarino", 1)
+
+        self.casillero = [
+            [Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla(por1), Casilla(por1), Casilla(por1), Casilla(por1), Casilla(por1), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla(fra1), Casilla(fra1), Casilla(fra1), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla(sub1), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla(fra2), Casilla(fra2), Casilla(fra2), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla(fra3), Casilla(fra3), Casilla(fra3), Casilla('agua'), Casilla('agua'), Casilla(sub3), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua')],
+            [Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla(sub4), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla('agua'), Casilla(sub2)]
+        ]
